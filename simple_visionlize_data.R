1e6bbb2d0d67bdc13bf57a4d71fa5a3b350850e0
farrah12 <farrazh12@gmail.com>
2017-01-09 09:24:18 -0800

    Check in initial R codes

---
+++ simple_visionlize_data.R
@@ -0,0 +1,6 @@
+library(readxl)
+
+business.data <- readRDS("yelp_data/business_LasVegas_Chinese_Restaurants.rds") 
+review.data <- readRDS("yelp_data/review_LasVegas_Chinese_Restaurants.rds")
+user.data <- readRDS("yelp_data/user_LasVegas_Chinese_Restaurants.rds")
+
